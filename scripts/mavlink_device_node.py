6421c04aa77228218c3f07daaa03cb4d13a60539
FaHa <dev-faha@t-online.de>
2017-10-21 00:11:52 +0200

    changed topic names

--- scripts/mavlink_device_node.py
+++ scripts/mavlink_device_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #===============================================================================
-# 
+#
 #===============================================================================
 
 import rospy
@@ -8,36 +8,36 @@ import serial
 from mavlink_phoenix.msg import MAV_RAW_DATA
 
 #===============================================================================
-# 
+#
 #===============================================================================
 ser = serial.Serial('/dev/senseboard', 115200, timeout=1)
 
 #===============================================================================
-# 
+#
 #===============================================================================
 def to_mav_raw_callback(message):
     rospy.loginfo("Write to mav "+ str(len(message.data)) + " bytes")
     ser.write(message.data)
-    
+
 #===============================================================================
-# 
+#
 #===============================================================================
 def node():
     rospy.init_node('mavlink_device_node', anonymous=True)
-    
-    pub = rospy.Publisher('/from_mav_mav_raw_data', MAV_RAW_DATA, queue_size=10)
-    rospy.Subscriber("/to_mav_mav_raw_data", MAV_RAW_DATA, to_mav_raw_callback,queue_size=10)
-    
-    r = rospy.Rate(1000) 
-    
+
+    pub = rospy.Publisher('/from_mav/mav_raw_data', MAV_RAW_DATA, queue_size=10)
+    rospy.Subscriber("/to_mav/mav_raw_data", MAV_RAW_DATA, to_mav_raw_callback,queue_size=10)
+
+    r = rospy.Rate(1000)
+
     m = MAV_RAW_DATA()
     m.channel=0
-    
+
     while not rospy.is_shutdown():
-      m.data = ser.read(255) 
+      m.data = ser.read(255)
       pub.publish(m)
       r.sleep()
-      
+
 if __name__ == '__main__':
     try:
         node()
